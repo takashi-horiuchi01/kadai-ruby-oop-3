3f6c2a55ccc027989a1f5071dd257d806f2b0f1b
takashi.horiuchi <takashi.horiuchi01@gmail.com>
2021-12-09 21:20:47 +0000

    task kadai-ruby-oop-3

---
+++ human.rb
@@ -0,0 +1,24 @@
+require './animal'
+require './thinkable'
+
+class Human < Animal
+  include Thinkable
+  attr_accessor :hobby
+
+  def initialize(name, age, hobby)
+    super(name, age)
+    self.hobby = hobby
+  end
+  
+  def koredeiino
+    think(self.hobby)
+  end
+
+end
+
+# test
+=begin
+tanaka = Human.new('田中 太郎',25,'電車')
+tanaka.say
+tanaka.koredeiino
+=end
---
+++ thinkable.rb
@@ -0,0 +1,10 @@
+module Thinkable
+  def think(hobby)
+    puts "私は#{hobby}について考えています。"
+
+# test
+#  def think
+#    puts "私はtestについて考えています。"
+
+  end
+end
